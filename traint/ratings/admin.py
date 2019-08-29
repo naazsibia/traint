75a4cf6731ee7ea564166bdf629dfe7dd736782a
Naaz Sibia <sibianjali@gmail.com>
2019-08-29 17:51:39 -0400

    Add working admin models

--- traint/ratings/admin.py
+++ traint/ratings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Rating, Dress
 
 # Register your models here.
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Dress)
+class DressAdmin(admin.ModelAdmin):
+    pass
+
+#admin.site.register(Rating, RatingAdmin)
+#admin.site.register(Dress, DressAdmin)
